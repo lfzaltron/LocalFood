9c466717079ae1364feb6cd22d4e154e8ae36b5e
Luis Fernando Zaltron <lfzaltron@gmail.com>
2021-02-28 11:28:50 -0300

    Distance between 2 points

--- src/pages/Filters/index.tsx
+++ src/pages/Filters/index.tsx
@@ -7,7 +7,11 @@ import Button from '../../components/Button';
 import { Container } from './styles';
 
 const Filters: React.FC = () => {
-  const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
+  const [location, setLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+    distancia: 0,
+  });
   const handleCurrentLocationPressed = useCallback(async () => {
     const result = await Geolocation.requestAuthorization('whenInUse');
     console.log({ result });
@@ -16,7 +20,13 @@ const Filters: React.FC = () => {
       position => {
         console.log(position);
         const { latitude, longitude } = position.coords;
-        setLocation({ latitude, longitude });
+
+        const d = distance(
+          { latitude, longitude },
+          { latitude: -30.0429672, longitude: -51.217359 },
+        );
+
+        setLocation({ latitude, longitude, distancia: d });
       },
       error => {
         console.log(error.code, error);
@@ -42,8 +52,29 @@ const Filters: React.FC = () => {
         Longitude:
         {location.longitude}
       </Text>
+      <Text>
+        Distancia:
+        {location.distancia}
+      </Text>
     </Container>
   );
 };
 
 export default Filters;
+
+function distance(
+  p1: { latitude: number; longitude: number },
+  p2: { latitude: number; longitude: number },
+) {
+  const R = 6371e3; // metres
+  const φ1 = (p1.latitude * Math.PI) / 180; // φ, λ in radians
+  const φ2 = (p2.latitude * Math.PI) / 180;
+  const Δφ = ((p2.latitude - p1.latitude) * Math.PI) / 180;
+  const Δλ = ((p2.longitude - p1.longitude) * Math.PI) / 180;
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // in metres
+  return d;
+}
